54c4d36dcdca1fb21de5c521eacc449c3e91fc10
eduardo.cruz <tecnologiaesolucoespr@gmail.com>
2025-01-16 13:33:49 -0300

    fix: vercel

--- dist/utils/jwtManager.js
+++ dist/utils/jwtManager.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createToken = createToken;
 exports.verifyToken = verifyToken;
-const encoding = process.env.TOKEN_JWT;
-const expiresIn = process.env.TOKEN_EXP;
+const encoding = '31990dbc-458d-4441-b279-5da3a75fd76e';
+const expiresIn = '24h';
 const jwt = require('jsonwebtoken');
 function createToken(payload) {
     return jwt.sign(payload, encoding, { expiresIn: expiresIn });
